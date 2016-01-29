ba12a61bacca03d8159bd3290b7c38345300ffe5
Alex <maddogmcclure@gmail.com>
2016-01-29 14:16:15 -0500

    Added click events and menus to ActivityLifeCycle

--- ActivityLifecycle/app/src/main/java/com/sandhillsoftware/activitylifecycle/Activity3.java
+++ ActivityLifecycle/app/src/main/java/com/sandhillsoftware/activitylifecycle/Activity3.java
@@ -2,8 +2,46 @@ package com.sandhillsoftware.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Activity3 extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity3, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Boolean handled = true;
+
+        switch (item.getItemId()) {
+            case R.id.action_showToast:
+                onMenuClickShowToast(item);
+                break;
+            case R.id.action_close:
+                onMenuClickClose(item);
+                break;
+            default:
+                handled = super.onOptionsItemSelected(item);
+                break;
+        }
+
+        return handled;
+    }
+
+    private void onMenuClickClose(MenuItem item) {
+        finish();
+    }
+
+    private void onMenuClickShowToast(MenuItem item) {
+        Toast t = Toast.makeText(this, getString(R.string.thisIsAToastText), Toast.LENGTH_LONG);
+        t.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- ActivityLifecycle/app/src/main/java/com/sandhillsoftware/activitylifecycle/MainActivity.java
+++ ActivityLifecycle/app/src/main/java/com/sandhillsoftware/activitylifecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sandhillsoftware.activitylifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -41,12 +42,37 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        boolean handled = true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_showActivity2:
+                onMenuClickShowActivity2(item);
+                break;
+            case R.id.action_showActivity3:
+                onMenuClickShowActivity3(item);
+                break;
+            case R.id.action_exit:
+                onMenuClickExit(item);
+                break;
+            default:
+                handled = super.onOptionsItemSelected(item);
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        return handled;
+    }
+
+    void onMenuClickShowActivity2(MenuItem item) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
+    void onMenuClickShowActivity3(MenuItem item) {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+
+    void onMenuClickExit(MenuItem item) {
+        finish();
     }
 }
